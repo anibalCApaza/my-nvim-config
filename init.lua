ebf47395664098ad2c4018cae70bd9bf93044b88
Dell arca <arcacorini@gmail.com>
2025-05-06 00:20:35 -0400

    Agregada coloración de modos

--- init.lua
+++ init.lua
@@ -6,10 +6,17 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true
 
 -- Evita que Neovim cambie el fondo de la terminal
-vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE") -- también en ventanas inactivas
+vim.cmd [[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight VertSplit guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+]]
 
 -- PLUGINS
 require("config.lazy")
 
 vim.cmd.colorscheme "catppuccin-macchiato"
+
+require('lualine').setup()
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -16,6 +16,10 @@ return {
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 -- refer to the configuration section below
+                        }
+        },
+        {
+                'nvim-lualine/lualine.nvim',
+                dependencies = { 'nvim-tree/nvim-web-devicons' }
         }
-}
 }
