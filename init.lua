55d0c4e0b0fa2ec44214ab75a0216c702623138c
arca fedora <arcacorini@gmail.com>
2025-05-06 13:20:11 -0400

    Agregado plugin tree-nvim y configuración de tabulaciones

--- init.lua
+++ init.lua
@@ -3,6 +3,10 @@ vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
 -- Evita que Neovim cambie el fondo de la terminal
 vim.cmd [[
   highlight Normal guibg=NONE ctermbg=NONE
@@ -18,3 +22,18 @@ require("config.lazy")
 vim.cmd.colorscheme "catppuccin-macchiato"
 
 require('lualine').setup()
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 25,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,7 +1,11 @@
 -- TODO: Implementar más plugins útiles
 
 return {
-        { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+        -- Colorscheme
+        { 
+                "catppuccin/nvim", name = "catppuccin", priority = 1000 
+        },
+        -- Autopairs to brackets
         {
                 'windwp/nvim-autopairs',
                 event = "InsertEnter",
@@ -9,6 +13,7 @@ return {
                 -- use opts = {} for passing setup options
                 -- this is equivalent to setup({}) function
         },
+        -- Highlights to Comments and filter/search
         {
                 "folke/todo-comments.nvim",
                 dependencies = { "nvim-lua/plenary.nvim" },
@@ -18,8 +23,14 @@ return {
                 -- refer to the configuration section below
                         }
         },
+        -- Upgrade to down bar
         {
                 'nvim-lualine/lualine.nvim',
                 dependencies = { 'nvim-tree/nvim-web-devicons' }
+        },
+        -- File Explorer
+        {
+                'nvim-tree/nvim-tree.lua'
         }
+
 }
