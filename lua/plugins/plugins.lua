aa0399c638c9ff9d2ee8010e22feab49f9a08609
arca fedora <arcacorini@gmail.com>
2025-05-08 11:13:14 -0400

    Formato aplicado en plugins.lua

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,36 +1,36 @@
 -- TODO: Implementar más plugins útiles
 
 return {
-        -- Colorscheme
-        { 
-                "catppuccin/nvim", name = "catppuccin", priority = 1000 
-        },
-        -- Autopairs to brackets
-        {
-                'windwp/nvim-autopairs',
-                event = "InsertEnter",
-                config = true
-                -- use opts = {} for passing setup options
-                -- this is equivalent to setup({}) function
-        },
-        -- Highlights to Comments and filter/search
-        {
-                "folke/todo-comments.nvim",
-                dependencies = { "nvim-lua/plenary.nvim" },
-                opts = {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-                        }
-        },
-        -- Upgrade to down bar
-        {
-                'nvim-lualine/lualine.nvim',
-                dependencies = { 'nvim-tree/nvim-web-devicons' }
-        },
-        -- File Explorer
-        {
-                'nvim-tree/nvim-tree.lua'
+    -- Colorscheme
+    {
+        "catppuccin/nvim", name = "catppuccin", priority = 1000
+    },
+    -- Autopairs to brackets
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    },
+    -- Highlights to Comments and filter/search
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
         }
+    },
+    -- Upgrade to down bar
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    -- File Explorer
+    {
+        'nvim-tree/nvim-tree.lua'
+    }
 
 }
