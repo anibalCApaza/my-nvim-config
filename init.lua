1471c93d9690ed52a017cdadaf50d22359dc6c8b
arca fedora <arcacorini@gmail.com>
2025-05-08 11:08:41 -0400

    Formato aplicado en init.lua

--- init.lua
+++ init.lua
@@ -30,26 +30,26 @@ vim.opt.termguicolors = true
 
 -- FUNCTIONS
 local function my_on_attach(bufnr)
-  local api = require "nvim-tree.api"
+    local api = require "nvim-tree.api"
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
 
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
 
-  -- custom mappings
-  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
-  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+    -- custom mappings
+    vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
 end
 
 
 -- MAPEO DE TECLAS
 -- En init.lua o en tu archivo de mappings
-vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" }) -- Abre/cierra nvim-tree con Ctrl+e
+vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })   -- Abre/cierra nvim-tree con Ctrl+e
 vim.keymap.set("n", "<C-r>", ":NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" }) -- Recarga nvim-tree con Ctrl+r
-vim.keymap.set("n", "<C-f>", ":NvimTreeFocus<CR>", { desc = "Focus File Explorer" })   -- Enfoca nvim-tree con Ctrl+f
+vim.keymap.set("n", "<C-f>", ":NvimTreeFocus<CR>", { desc = "Focus File Explorer" })     -- Enfoca nvim-tree con Ctrl+f
 
 -- PLUGINS
 require("config.lazy")
@@ -59,20 +59,20 @@ vim.cmd.colorscheme "catppuccin-macchiato"
 require('lualine').setup({})
 
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 25,
-    side = "right"
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-  on_attach = my_on_attach,
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 25,
+        side = "right"
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+    on_attach = my_on_attach,
 })
 
 
