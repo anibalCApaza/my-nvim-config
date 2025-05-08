99527c03b68148417c01864c95a3c0926066ea29
arca fedora <arcacorini@gmail.com>
2025-05-08 11:08:18 -0400

    Agregado mapeado de tecla para aplicar formato/ aplicacion de formato en lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,20 +2,22 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
         "williamboman/mason.nvim",
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-            library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-      },
-  },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
     },
     config = function()
-
         local on_attach = function(_, bufnr)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = bufnr})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+            vim.keymap.set('n', '<leader>f', function()
+                vim.lsp.buf.format { async = true }
+            end)
         end
 
         require("lazydev").setup({})
@@ -23,41 +25,40 @@ return {
             on_attach = on_attach,
 
 
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if
-        path ~= vim.fn.stdpath('config')
-        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-      then
-        return
-      end
-    end
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if
+                        path ~= vim.fn.stdpath('config')
+                        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+                    then
+                        return
+                    end
+                end
 
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        version = 'LuaJIT',
-        path = {
-          'lua/?.lua',
-          'lua/?/init.lua',
-        },
-      },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        version = 'LuaJIT',
+                        path = {
+                            'lua/?.lua',
+                            'lua/?/init.lua',
+                        },
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                        }
+                    }
+                })
+            end,
+            settings = {
+                Lua = {
+                    telemetry = { enable = true },
+                    workspace = { checkThirdParty = false }
+                }
+            }
         }
-      }
-    })
-  end,
-  settings = {
-    Lua = {
-        telemetry = { enable = true },
-        workspace = { checkThirdParty = false }
-    }
-  }
-  }
         )
     end
 }
-
