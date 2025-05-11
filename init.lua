d8c3f4488a4b2db63c176f545389e301e21bdd0e
Dell arca <arcacorini@gmail.com>
2025-05-11 02:33:33 -0400

    Configuración de colorscheme kanagawa

--- init.lua
+++ init.lua
@@ -92,6 +92,34 @@ if vim.fn.has('unix') == 1 then
     vim.cmd.colorscheme "catppuccin"
 elseif vim.fn.has('win64') == 1 then
     vim.cmd.colorscheme("kanagawa")
+    -- Default options:
+    require('kanagawa').setup({
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = false },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = {         -- add/modify theme and palette colors
+            palette = {},
+            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+            return {}
+        end,
+        theme = "wave", -- Load "wave" theme
+        background = { -- map the value of 'background' option to a theme
+            dark = "wave", -- try "dragon" !
+            light = "lotus"
+        },
+    })
+
+    -- setup must be called before loading
+    vim.cmd("colorscheme kanagawa")
 else
     vim.cmd.colorscheme("default")
 end
