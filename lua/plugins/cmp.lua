9642d734e616d5ec9c289d283b2bf0fa848d0d56
arca fedora <arcacorini@gmail.com>
2025-05-11 11:46:55 -0400

    Agregando dependencias para cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,5 +7,9 @@ return {
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
         'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-git',
+        'L3MON4D3/LuaSnip',
+        'hrsh7th/nvim-cmp',
+        'windwp/nvim-autopairs'
     }
 }
