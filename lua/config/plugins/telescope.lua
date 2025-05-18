bee1fffef43c91428771014622613d685844b59f
Dell arca <arcacorini@gmail.com>
2025-05-18 14:11:36 -0400

    Mejora en la configuracion de telescopa, teniendo carga lenta

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,6 +1,7 @@
 local M = {}
-
+local telescope = require('telescope')
 function M.setup()
+    telescope.setup({})
 end
 
 return M
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
+    event = 'VeryLazy',
     dependencies = { 'nvim-lua/plenary.nvim' },
     main = "config.plugins.telescope",
     config = true
