14d51f66125575a53637f10a6765ffe14a7218c4
Dell arca <arcacorini@gmail.com>
2025-05-18 14:53:30 -0400

    Implementación de busquedas rápidas usando C con fzf para telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,8 +2,13 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     event = 'VeryLazy',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make'
+        }
+    },
     main = "config.plugins.telescope",
     config = true
-
 }
