b203f290c539b2c57558ac1b52bcb1c9aa4fe105
arca fedora <arcacorini@gmail.com>
2025-05-22 17:41:44 -0400

    Inhabilitando busqueda de telescope en archivos de dependencias

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -2,6 +2,20 @@ local M = {}
 local telescope = require('telescope')
 function M.setup()
     telescope.setup({
+        defaults = {
+            file_ignore_patterns = {
+                "node_modules", -- ignora node_modules
+                "^%.git/",      -- ignora carpeta .git
+                "^vendor/",     -- ignora vendor de PHP/Laravel
+                "^%.venv/",     -- ignora .venv
+                "^venv/",       -- ignora venv sin punto
+                "__pycache__/", -- ignora cache de Python
+                "%.lock",       -- yarn.lock, package-lock.json, etc.
+                "%.env",        -- archivos .env, .env.local, etc.
+                "%.min%.js",    -- JS minificados
+                "%.min%.css",   -- CSS minificados
+            }
+        },
         extensions = {
             fzf = {
                 fuzzy = true,                   -- false will only do exact matching
