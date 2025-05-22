58ab82d8799030ecf22b6d897c8f1db7bed589c3
arca fedora <arcacorini@gmail.com>
2025-05-22 17:29:44 -0400

    Agregando autocmd de yanked eliminado por error, reemplazando el autocmd de LSP correctamente

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,33 +2,6 @@ local api = vim.api
 
 
 -- LSP
-api.nvim_create_autocmd("LspProgress", {
-    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-    callback = function(ev)
-        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-        vim.notify(vim.lsp.status(), vim.log.levels.INFO, {
-            id = "lsp_progress",
-            title = "LSP Progress",
-            opts = function(notif)
-                notif.icon = ev.data.params.value.kind == "end" and " "
-                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-            end,
-        })
-    end,
-})
-
-
--- Notify when a buffer is written (saved)
--- Shows the name of the file that was just saved
-api.nvim_create_autocmd("BufWritePost", {
-    callback = function()
-        vim.notify("File saved: " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
-    end,
-})
-
-
--- Notify when text is yanked (copied)
--- Useful visual feedback for copy operations
 ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
 local progress = vim.defaulttable()
 vim.api.nvim_create_autocmd("LspProgress", {
@@ -62,7 +35,7 @@ vim.api.nvim_create_autocmd("LspProgress", {
     end, p)
 
     local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-    vim.notify(table.concat(msg, "\n"), "info", {
+    vim.notify(table.concat(msg, "\n"), vim.log.levels.INFO, {
       id = "lsp_progress",
       title = client.name,
       opts = function(notif)
@@ -72,3 +45,24 @@ vim.api.nvim_create_autocmd("LspProgress", {
     })
   end,
 })
+
+
+-- Notify when a buffer is written (saved)
+-- Shows the name of the file that was just saved
+api.nvim_create_autocmd("BufWritePost", {
+    callback = function()
+        vim.notify("File saved: " .. vim.fn.expand("%:t"), vim.log.levels.INFO)
+    end,
+})
+
+
+-- Notify when text is yanked (copied)
+-- Useful visual feedback for copy operations
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.notify("Text copied to clipboard", vim.log.levels.INFO)
+    end,
+})
+
+
+
