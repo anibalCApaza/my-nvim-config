09f1103dc1f225183acad247d7bb8750e97b09ed
arca fedora <arcacorini@gmail.com>
2025-05-29 00:02:01 -0400

    Habilitando Prettier para typescrupt

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -8,6 +8,7 @@ function M.setup()
             html = { 'prettier' },
             css = { 'prettier' },
             javascript = { 'prettier' },
+            typescript = { 'prettier' }
         }
     })
 end
